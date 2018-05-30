6079471182215fc4832db3fc0e913b58b771e3f1
Eddie Kao <eddie@5xruby.tw>
2018-05-30 17:22:10 +0800

    move "charge" method to Movie class

--- lib/movie.rb
+++ lib/movie.rb
@@ -9,5 +9,21 @@ class Movie
   def initialize(title, price_code)
     @title, @price_code = title, price_code
   end
+
+  def charge(days_rented)
+    this_amount = 0
+    case price_code
+    when REGULAR
+      this_amount += 2
+      this_amount += (days_rented - 2) * 1.5 if days_rented > 2
+    when NEW_RELEASE
+      this_amount += days_rented * 3
+    when CHILDRENS
+      this_amount += 1.5
+      this_amount += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+
+    this_amount
+  end
 end
 
--- lib/rental.rb
+++ lib/rental.rb
@@ -6,19 +6,7 @@ class Rental
   end
 
   def charge
-    this_amount = 0
-    case movie.price_code
-    when Movie::REGULAR
-      this_amount += 2
-      this_amount += (days_rented - 2) * 1.5 if days_rented > 2
-    when Movie::NEW_RELEASE
-      this_amount += days_rented * 3
-    when Movie::CHILDRENS
-      this_amount += 1.5
-      this_amount += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-
-    this_amount
+    movie.charge(days_rented)
   end
 
   def frequent_renter_points
