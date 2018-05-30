679ddb86fc0b8a2bb48275db279e068d018857a7
Eddie Kao <eddie@5xruby.tw>
2018-05-30 17:20:54 +0800

    add "total_frequent_renter_points" method and replace local variable

--- lib/customer.rb
+++ lib/customer.rb
@@ -11,25 +11,25 @@ class Customer
   end
 
   def statement
-    frequent_renter_points = 0  # 常客積點
     result = "Rental Record for #{@name}\n"
 
     @rentals.each do |rental|
-      # 累加常客積點
-      frequent_renter_points += rental.frequent_renter_points
-
       # 顯示此筆租借資料
       result += "\t" + rental.movie.title + "\t" + rental.charge.to_s + "\n"
     end
 
     # 結尾列印
     result += "Amount owed is #{total_charge}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
+    result += "You earned #{total_frequent_renter_points} frequent renter points"
     result
   end
 
   private
   def total_charge
     @rentals.reduce(0) { |sum, rental| sum + rental.charge }
+  end
+
+  def total_frequent_renter_points
+    @rentals.reduce(0) { |sum, rental| sum + rental.frequent_renter_points }
   end
 end
