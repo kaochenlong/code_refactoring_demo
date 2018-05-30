f9deff4102ddde2a41daf3e96b5ea05d1e03d1bb
Eddie Kao <eddie@5xruby.tw>
2018-05-30 17:34:50 +0800

    add price_code setter

--- lib/movie.rb
+++ lib/movie.rb
@@ -26,10 +26,24 @@ class Movie
   CHILDRENS   = 2     # 兒童片
 
   attr_reader :title
-  attr_accessor :price_code
+  attr_reader :price_code
 
   def initialize(title, price_code)
-    @title, @price_code = title, price_code
+    @title = title
+    self.price_code = price_code
+  end
+
+  def price_code=(code)
+    @price_code = code
+
+    case @price_code
+    when REGULAR
+      @price = RegularPrice.new
+    when NEW_RELEASE
+      @price = NewReleasePrice.new
+    when CHILDRENS
+      @price = ChildrenPrice.new
+    end
   end
 
   def charge(days_rented)
